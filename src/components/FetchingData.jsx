624493ed0493c3ac659dc1a949a17766440d7fcc
Ahmad Ali <xevenahmad@gmail.com>
2024-11-29 22:31:04 +0500

    styled react bootstrap table

--- src/components/FetchingData.jsx
+++ src/components/FetchingData.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
-
+import Table from 'react-bootstrap/Table';
+import 'bootstrap/dist/css/bootstrap.css';
 const FetchingData = () => {
   const [data, setData] = useState([]);
   console.log('first::::::::', data);
@@ -8,12 +9,35 @@ const FetchingData = () => {
       .then((res) => res.json())
       .then((result) => {
         console.log('result::', result); // Log the fetched data
-        setData(result);     // Correctly update state
+        setData(result); // Correctly update state
       });
     //   .then((data) => setData(data));
   }, []);
 
-  return <div>Hello</div>;
+  return (
+    <div className='parent_fetch_data_component'>
+      <Table striped bordered hover>
+        <thead style={{ border: '5px solid #AAFF00' }}>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>User Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody style={{ border: '5px solid #B65FCF' }}>
+          {data.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.username}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  );
 };
 
 export default FetchingData;
